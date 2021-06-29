0ee8f6f4aefab551270fd5f9920f278e28e43d6c
Omowunmi <omowunmi.ekun@xputertechnologies.com>
2021-06-29 16:04:40 +0100

    node_tuto

---
+++ node_tutorial.js
@@ -0,0 +1,15 @@
+var url = require('url');
+var http = require('http');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr,true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+var qdata = q.query;
+console.log(qdata)
+http.createServer((req,res)=> {
+    console.log(req,res);
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end('Hello Wunmi');
+}).listen(8080);
